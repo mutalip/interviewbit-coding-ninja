e9b85359b18893e84f49ff9ef0947192192d988e
Mutalip Kurban <mutalip.kurban@gmail.com>
2020-02-12 22:05:35 -0800

    Create MinStepsInInfiniteGrid.py

---
+++ 02Arrays/MinStepsInInfiniteGrid.py
@@ -0,0 +1,43 @@
+# Min Steps in Infinite Grid
+# Asked in:  
+# Directi
+# You are in an infinite 2D grid where you can move in any of the 8 directions :
+
+#  (x,y) to 
+#     (x+1, y), 
+#     (x - 1, y), 
+#     (x, y+1), 
+#     (x, y-1), 
+#     (x-1, y-1), 
+#     (x+1,y+1), 
+#     (x-1,y+1), 
+#     (x+1,y-1) 
+# You are given a sequence of points and the order in which you need to cover the points. Give the minimum number of steps in which you can achieve it. You start from the first point.
+
+# Input :
+
+# Given two integer arrays A and B, where A[i] is x coordinate and B[i] is y coordinate of ith point respectively.
+# Output :
+
+# Return an Integer, i.e minimum number of steps.
+# Example :
+
+# Input : [(0, 0), (1, 1), (1, 2)]
+# Output : 2
+# It takes 1 step to move from (0, 0) to (1, 1). It takes one more step to move from (1, 1) to (1, 2).
+
+# This question is intentionally left slightly vague. Clarify the question by trying out a few cases in the “See Expected Output” section.
+class Solution:
+    # @param A : list of integers
+    # @param B : list of integers
+    # @return an integer
+    def coverPoints(self, A, B):
+        length = len(A)
+        steps = 0
+        for i in range(length-1):
+            x = abs(A[i]- A[i+1])
+            y = abs(B[i]-B[i+1])
+            
+            steps += max(x,y)
+       
+        return steps
