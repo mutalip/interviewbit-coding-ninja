09483e1086e463e15a7b70d4fef1fe098049d400
Mutalip Kurban <mutalip.kurban@gmail.com>
2020-03-06 15:02:07 -0800

    Create MergeOverlappingIntervals.py

---
+++ 02Arrays/MergeOverlappingIntervals.py
@@ -0,0 +1,36 @@
+
+# Merge Overlapping Intervals
+
+#     Asked in:  
+#     Google
+#     Amazon
+
+# Given a collection of intervals, merge all overlapping intervals.
+
+# For example:
+
+# Given [1,3],[2,6],[8,10],[15,18],
+
+# return [1,6],[8,10],[15,18].
+
+# Make sure the returned intervals are sorted.
+# Seen this question in a real interview before
+# Definition for an interval.
+# class Interval:
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution:
+    # @param intervals, a list of Intervals
+    # @return a list of Interval
+    #def merge(self, intervals):
+    def merge(self, I):
+        res = []
+        I.sort(key=lambda i: i.start)
+        for i in I:
+            if not res or res[-1].end < i.start:
+                res.append(i)
+            else:
+                res[-1].end = max(res[-1].end, i.end)
+        return res 
